7e4b40fe52a5366d0cd0369d974fa6dc8b8f5927
Pedro Gomes <pedroleinar@hotmail.com>
2020-06-03 01:39:00 -0300

    seeds

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,4 +6,4 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log('server started'));
+app.listen(3333, () => console.log('🚀 Server started on port 3333'));
