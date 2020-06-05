e6c2bc5da92b6dd0492954dc5007882d52ecca4f
Pedro Gomes <pedroleinar@hotmail.com>
2020-06-05 05:22:59 -0300

    map-leaflet

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import logo from '../../assets/logo.svg';
 
@@ -50,6 +51,15 @@ const CreatePoint = () => {
             <h2>Endereço</h2>
             <span>Selecione o endereço no mapa</span>
           </legend>
+
+          <Map center={[-23.981558, -46.2119957]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[-23.981558, -46.2119957]} />
+          </Map>
 
           <div className="field-group">
             <div className="field">
