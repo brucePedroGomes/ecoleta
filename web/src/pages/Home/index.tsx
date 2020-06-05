f1f5fd5720acf5f1af72ab9d83a1fd7dda879ef8
Pedro Gomes <pedroleinar@hotmail.com>
2020-06-05 04:16:40 -0300

    home-done

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import logo from '../../assets/logo.svg';
+import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
 import './styles.css';
 
 const Home = () => {
   return (
     <div id="page-home">
       <div className="content">
-        <img src={logo} alt="Ecoleta" />
+        <header>
+          <img src={logo} alt="Ecoleta" />
+        </header>
+
+        <main>
+          <h1>Seu marketplace de coleta de resíduos</h1>
+          <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+
+          <Link to="/create-point">
+            <span>
+              <FiLogIn />
+            </span>
+            <strong>Cadastre um ponto de coleta</strong>
+          </Link>
+        </main>
       </div>
     </div>
   );
