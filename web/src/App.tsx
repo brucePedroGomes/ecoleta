6c3f70b423906af230f78fb1de9aeca004c49739
Pedro Gomes <pedroleinar@hotmail.com>
2020-06-05 03:21:58 -0300

    init-home

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
 import './App.css';
 
+import Home from './pages/Home';
+
 function App() {
-  return <h1>Hello world</h1>;
+  return <Home />;
 }
 
 export default App;
