c158d39bcf342f1f53e31132664287b8bf652d8d
Pedro Gomes <pedroleinar@hotmail.com>
2020-06-08 02:00:03 -0300

    validation

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { errors } from 'celebrate';
+
 import routes from './routes';
 import path from 'path';
 import cors from 'cors';
@@ -10,5 +12,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333, () => console.log('🚀 Server started on port 3333'));
